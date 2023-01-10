9bb8bb515f178c757621c698b929e3692bbfcb8b
BuildTools <unconfigured@null.spigotmc.org>
2023-01-09 22:17:31 -0600

    Updated files to make the names more readable when referencing and coding.

--- src/test/java/me/uwuspeak/UspeakPluginTest.java
+++ src/test/java/me/uwuspeak/UspeakPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package me.uwuspeak;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class UspeakPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(UspeakPlugin.class);
 		RuneLite.main(args);
 	}
 }
